765c90eabbdaaeab8653b97098422af26d3ea63a
wata <w.nagasawa@gmail.com>
2014-02-23 01:46:35 +0900

    メタタグ、画像、スタイル調整、その他

--- Gruntfile.js
+++ Gruntfile.js
@@ -180,7 +180,7 @@ module.exports = function(grunt){
             '<?xml version="1.0" encoding="UTF-8"?>\n'
             + '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
             + '    <url>\n'
-            + '        <loc><%= pkg.homepage %>/</loc>\n'
+            + '        <loc>http://indstria.com/</loc>\n'
             + '        <lastmod>' + new Date() + '</lastmod>\n'
             + '    </url>\n'
             + '</urlset>'
